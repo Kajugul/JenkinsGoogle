506d5cf0abe8c4849f33cec1abff4d285a34ca9d
Sandeep Duve <sandeep.duve@gmail.com>
2020-02-02 18:06:41 +0530

    Initial commit

---
+++ src/main/java/JenkinsTesting.java
@@ -0,0 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class JenkinsTesting
+{
+    public static void main(String[] a)
+    {
+        System.out.println("Initialing the browser for you....Please wait...");
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(40000, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.google.com/");
+
+       // Thread.sleep(3000);
+        System.out.println("Closing the browser....");
+        driver.quit();
+    }
+}
