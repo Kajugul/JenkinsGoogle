369cf4e578619cbc2c208bff12f4e3a6fb4eadd7
Sandeep Duve <sandeep.duve@gmail.com>
2020-03-29 23:55:49 +0530

    d

--- src/test/java/TestCases/FacebookTesting.java
+++ src/test/java/TestCases/FacebookTesting.java
@@ -25,42 +25,6 @@ public class FacebookTesting
     {
         extent = ExtentTestManager.startTest(method.getName(), "verifytitle");
 
-        extent.log(LogStatus.INFO, "initiating webdriver");
-        //WebDriverManager.chromedriver().setup();
-        System.out.println("Initialing the browser for you....Please wait...");
-        String chromeDriverPath = System.getProperty("user.dir") + "//Datafiles//chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-        driver = new ChromeDriver();
-       // driver = new ChromeDriver();
-        extent.log(LogStatus.INFO, "webdriver initiated successfully.");
-
-        extent.log(LogStatus.INFO, "maximizing the window");
-        driver.manage().window().maximize();
-        extent.log(LogStatus.INFO, "window maximizing successfully.");
-
-        driver.manage().timeouts().pageLoadTimeout(40000, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-        extent.log(LogStatus.INFO, "launching the google site");
-        driver.get("https://www.facebook.com/");
-        extent.log(LogStatus.INFO, "google site lauched successfully.");
-
-        extent.log(LogStatus.INFO, "verifing the title it should failed");
-        Assert.assertEquals(driver.getTitle(), "Google");
-        extent.log(LogStatus.INFO, "title verified successfully.");
-
-        Thread.sleep(2000);
     }
 
-    public WebDriver getWebDriver()
-    {
-        return driver;
-        // time pass
-    }
-
-    @AfterTest
-    public void close()
-    {
-        driver.quit();
-    }
 }
